7d3fefa403a81bc10ec0e4342a8d0db5284f075d
tejaswini-salunke <yadavtejaswini6@gmail.com>
2026-07-02 10:50:51 +0530

    Added

---
+++ CoreMVCApplicationWithCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreMVCApplicationWithCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
